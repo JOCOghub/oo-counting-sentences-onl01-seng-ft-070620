76825dddfbc07c902bdb7330ebaa07f8a192cf55
Joseph cohen <jojocohen57@gmail.com>
2020-07-15 22:21:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,5 +17,5 @@ class String
   def count_sentences
       self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size  
   end
-  
+  "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
 end
